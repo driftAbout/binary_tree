cf10cce936f55fc1c7973340157d55d1e7d72d73
Kevin MIller <kevin@driftabout.com>
2017-11-16 11:49:47 -0800

    basic recursive insert

--- app.js
+++ app.js
@@ -6,32 +6,59 @@ function BinaryTree(val){
   this.right;
 }
 
+// BinaryTree.prototype.insertValue = function(val) {
+//   if (val < this.value){
+//     if (! this.left){
+//       this.left = new BinaryTree(val);
+//       return;
+//     }
+//     if (val < this.left.value){
+//       if (! this.left.left){
+//         this.left.left = new BinaryTree(val);
+//         return;
+//       }
+//     }
+//   }
+//   if (val > this.value){
+//     if (! this.right){
+//       this.right = new BinaryTree(val);
+//       return;
+//     }
+//     if (val > this.right.value){
+//       if (! this.right.right){
+//         this.right.right = new BinaryTree(val);
+//         return;
+//       }
+//     }
+//   }
+// };
+
 BinaryTree.prototype.insertValue = function(val) {
-  if (val < this.value){
-    if (! this.left){
-      this.left = new BinaryTree(val);
+  var binaryObject = this;
+  var branch;
+  var traverse = true;
+  while (traverse){
+    //if the value exist leave the function
+    if (val === binaryObject.value){
+      traverse = false;
       return;
     }
-    if (val < this.left.value){
-      if (! this.left.left){
-        this.left.left = new BinaryTree(val);
-        return;
-      }
+    //set the branch to right, change it to left if is less than the value
+    branch = 'right';
+    if (val < binaryObject.value){
+      branch = 'left';
     }
-  }
-  if (val > this.value){
-    if (! this.right){
-      this.right = new BinaryTree(val);
+    //if the branch does not exist, create it with that value
+    if (! binaryObject[branch]){
+      binaryObject[branch] = new BinaryTree(val);
+      traverse = false;
       return;
     }
-    if (val > this.right.value){
-      if (! this.right.right){
-        this.right.right = new BinaryTree(val);
-        return;
-      }
-    }
+    //if the branch exists set it as the next branch to search
+    binaryObject = binaryObject[branch];
   }
 };
+
 
 var num = 157;
 var binary_tree = new BinaryTree(num);
